ceb625818504e95341b34902f3e00dc4bb4b466d
AvellPessoal <lincoln.araujo.dias@gmail.com>
2019-07-28 23:49:28 -0300

    initializedDotEnv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from "dotenv";
 
 async function bootstrap() {
+  await dotenv.load()
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.SERVER_PORT);
 }
