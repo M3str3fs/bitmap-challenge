008260655b1a2293bd1f2f9f18cbbc5fca4f046f
Lincoln Araujo Dias <jsmterrestre@gmail.com>
2019-07-29 10:06:08 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BfsExampleModule } from './bfs-example/bfs-example.module';
-import { BfsExampleController } from './bfs-example/bfs-example.controller';
-import { UsersService } from './users/users.service';
+
+
 
 @Module({
   imports: [BfsExampleModule],
